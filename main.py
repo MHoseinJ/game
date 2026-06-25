17a1d00a34cb211f4951553164d39893e16dfa93
MHoseinJ <mhmtsze@gmail.com>
2026-06-25 20:28:33 +0330

    fix: remove debug 'scripts' variable

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from src import functions
 from src import variables
 from src.scripts.snake import Snake
 
-scripts = [
-    Snake()
-]
-
 for script in scripts:
     script.start()
 
 
-variables.SCREEN: Surface = functions.init(variables.WIDTH, variables.HEIGHT, variables.TITLE)
+variables.SCREEN = functions.init(variables.WIDTH, variables.HEIGHT, variables.TITLE)
 
 running = True
 
