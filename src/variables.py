86832c0508820c17d6a48ae32e4bec4f4795cad7
MHoseinJ <mhmtsze@gmail.com>
2026-06-25 20:29:35 +0330

    feat: add scripts variable for user

--- src/variables.py
+++ src/variables.py
@@ -5,10 +5,13 @@ TITLE = "game python"
 
 SCREEN = 0
 
-
 COLORS = {
     "black": (0,0,0),
     "white": (255,255,255),
     "light_bg": (80, 255, 100),
     "dark_bg": (50, 150, 60)
 }
+
+SCRIPTS = [
+    
+]
