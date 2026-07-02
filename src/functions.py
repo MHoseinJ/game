47271187d253d0b31a78da346a60469e353cac02
MHoseinJ <mhmtsze@gmail.com>
2026-07-02 23:04:38 +0330

    fix: change signature of functions

--- src/functions.py
+++ src/functions.py
@@ -1,15 +1,17 @@
+from src.gameobject import GameObject
+from src.variables import GAMEOBJECTS
 import pygame
 
-def init(width: int, height: int, title: str):
+def init(width: int, height: int, block_size: int, title: str):
 
     pygame.init()
 
-    screen = pygame.display.set_mode([width, height])
+    screen = pygame.display.set_mode([width * block_size, height * block_size])
     pygame.display.set_caption(title)
 
     return screen
 
-def get_events(events):
+def get_events(events) -> list[str]:
 
     keys_down = []
 
@@ -20,3 +22,20 @@ def get_events(events):
             keys_down.append(pygame.key.name(event.key))
 
     return keys_down
+
+def call_start(scripts):
+
+    for script in scripts:
+        script.start()
+
+def call_update(scripts, dt):
+    
+    for script in scripts:
+        script.update(dt)
+
+def find_object(name: str, objects: list[GameObject] = GAMEOBJECTS) -> GameObject:
+    for object in objects:
+        if object.get_name() == name:
+            return object
+
+    return GameObject()
